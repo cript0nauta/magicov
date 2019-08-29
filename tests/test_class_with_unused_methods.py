db2bea563a0f536a13be8b89bcecaca3d2cebc03
Matías Lang <matiasl@infobytesec.com>
2019-08-29 18:15:40 -0300

    Improve function rewriting tests related to generator functions

--- tests/test_class_with_unused_methods.py
+++ tests/test_class_with_unused_methods.py
@@ -3,7 +3,22 @@ _magicov_expected_uncovered_lines = 3
 class Test:
     def method1(self):
         removeme
+
     def method2(self):
         removeme
+
     def method3(self):
         removeme
+
+    def unused_generator(self):
+        # This function is not covered at first because it has a yield,
+        # but will be covered after being rewritten.
+        n=1
+        removeme
+        yield 1
+
+    def used_generator(self):
+        yield 1
+
+Test().unused_generator()
+next(Test().used_generator())
